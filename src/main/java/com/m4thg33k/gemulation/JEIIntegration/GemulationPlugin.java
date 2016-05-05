93b12dd191ab89244f3fafa3ab06c3e1a3dcd045
M4thG33k <pepethemathgeek@gmail.com>
2016-05-05 15:32:05 -0500

    updated to make use of newest LIT features

--- src/main/java/com/m4thg33k/gemulation/JEIIntegration/GemulationPlugin.java
+++ src/main/java/com/m4thg33k/gemulation/JEIIntegration/GemulationPlugin.java
@@ -1,11 +1,13 @@
 package com.m4thg33k.gemulation.JEIIntegration;
 
+import com.m4thg33k.gemulation.blocks.ModBlocks;
 import com.m4thg33k.gemulation.items.ItemGemBag;
 import com.m4thg33k.gemulation.items.ModItems;
 import mezz.jei.api.BlankModPlugin;
 import mezz.jei.api.IJeiHelpers;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
+import net.minecraft.item.Item;
 
 import javax.annotation.Nonnull;
 
@@ -17,5 +19,9 @@ public class GemulationPlugin extends BlankModPlugin{
         IJeiHelpers jeiHelpers = registry.getJeiHelpers();
 
         jeiHelpers.getNbtIgnoreList().ignoreNbtTagNames(ModItems.itemGemBag,ItemGemBag.TAG_ITEMS,ItemGemBag.TAG_SLOT);
+        jeiHelpers.getNbtIgnoreList().ignoreNbtTagNames(Item.getItemFromBlock(ModBlocks.gemChestBlock),"Items");
+        jeiHelpers.getNbtIgnoreList().ignoreNbtTagNames(Item.getItemFromBlock(ModBlocks.darkGemChestBlock),"Items");
+        jeiHelpers.getNbtIgnoreList().ignoreNbtTagNames(Item.getItemFromBlock(ModBlocks.superGemChestBlock),"Items");
+        jeiHelpers.getNbtIgnoreList().ignoreNbtTagNames(Item.getItemFromBlock(ModBlocks.darkSuperGemChestBlock),"Items");
     }
 }
--- src/main/java/com/m4thg33k/gemulation/core/recipes/BagRecipe.java
+++ src/main/java/com/m4thg33k/gemulation/core/recipes/BagRecipe.java
@@ -2,91 +2,114 @@ package com.m4thg33k.gemulation.core.recipes;
 
 import com.m4thg33k.gemulation.items.ItemGemBag;
 import com.m4thg33k.gemulation.items.ModItems;
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 import net.silentchaos512.gems.block.ModBlocks;
 
-import java.util.ArrayList;
+public class BagRecipe extends ShapelessOreRecipe{
 
-public class BagRecipe implements IRecipe{
-
-
-    @Override
-    public boolean matches(InventoryCrafting inv, World worldIn) {
-        boolean foundBag =false;
-        boolean foundRose = false;
-        boolean foundNetherrack = false;
-        for (int i=0;i<inv.getSizeInventory();i++)
-        {
-            ItemStack stack = inv.getStackInSlot(i);
-            if (stack!=null)
-            {
-                if (foundBag && foundRose && foundNetherrack)
-                {
-                    return false;
-                }
-                if (stack.getItem() instanceof ItemGemBag)
-                {
-                    foundBag = true;
-                }
-                else if (stack.getItem() == Item.getItemFromBlock(ModBlocks.glowRose))
-                {
-                    foundRose = true;
-                }
-                else if (stack.getItem() == Item.getItemFromBlock(Blocks.netherrack))
-                {
-                    foundNetherrack = true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-        return foundBag&&foundRose;
+    public BagRecipe(Block result, Object... recipe)
+    {
+        super(result, recipe);
     }
+    public BagRecipe(Item result, Object... recipe)
+    {
+        super(result, recipe);
+    }
+    public BagRecipe(ItemStack result,Object... recipe)
+    {
+        super(result, recipe);
+    }
+
+
+//    @Override
+//    public boolean matches(InventoryCrafting inv, World worldIn) {
+//        boolean foundBag =false;
+//        boolean foundRose = false;
+//        boolean foundNetherrack = false;
+//        for (int i=0;i<inv.getSizeInventory();i++)
+//        {
+//            ItemStack stack = inv.getStackInSlot(i);
+//            if (stack!=null)
+//            {
+//                if (foundBag && foundRose && foundNetherrack)
+//                {
+//                    return false;
+//                }
+//                if (stack.getItem() instanceof ItemGemBag)
+//                {
+//                    foundBag = true;
+//                }
+//                else if (stack.getItem() == Item.getItemFromBlock(ModBlocks.glowRose))
+//                {
+//                    foundRose = true;
+//                }
+//                else if (stack.getItem() == Item.getItemFromBlock(Blocks.netherrack))
+//                {
+//                    foundNetherrack = true;
+//                }
+//                else
+//                {
+//                    return false;
+//                }
+//            }
+//        }
+//        return foundBag&&foundRose;
+//    }
 
     @Override
     public ItemStack getCraftingResult(InventoryCrafting inv) {
-        ItemStack bag = null;
-        int roseMeta = 0;
-        boolean netherrack = false;
+
+        ItemStack output = super.getCraftingResult(inv);
 
         for (int i=0;i<inv.getSizeInventory();i++)
         {
             ItemStack stack = inv.getStackInSlot(i);
-            if (stack!=null)
+            if (stack!=null && stack.getItem() instanceof ItemGemBag && stack.hasTagCompound())
             {
-                if (stack.getItem() instanceof ItemGemBag)
-                {
-                    bag = stack.copy();
-                }
-                else if (stack.getItem() == Item.getItemFromBlock(ModBlocks.glowRose))
-                {
-                    roseMeta = stack.getItemDamage();
-                }
-                else if (stack.getItem() == Item.getItemFromBlock(Blocks.netherrack))
-                {
-                    netherrack = true;
-                }
+                output.setTagCompound(stack.getTagCompound());
             }
         }
-        NBTTagCompound tagCompound = null;
-        if (bag.hasTagCompound())
-        {
-            tagCompound = bag.getTagCompound();
-        }
-        ItemStack output = new ItemStack(ModItems.itemGemBag,1,roseMeta+(netherrack?16:0));
-
-        if (tagCompound!=null)
-        {
-            output.setTagCompound(tagCompound);
-        }
+//        ItemStack bag = null;
+//        int roseMeta = 0;
+//        boolean netherrack = false;
+//
+//        for (int i=0;i<inv.getSizeInventory();i++)
+//        {
+//            ItemStack stack = inv.getStackInSlot(i);
+//            if (stack!=null)
+//            {
+//                if (stack.getItem() instanceof ItemGemBag)
+//                {
+//                    bag = stack.copy();
+//                }
+//                else if (stack.getItem() == Item.getItemFromBlock(ModBlocks.glowRose))
+//                {
+//                    roseMeta = stack.getItemDamage();
+//                }
+//                else if (stack.getItem() == Item.getItemFromBlock(Blocks.netherrack))
+//                {
+//                    netherrack = true;
+//                }
+//            }
+//        }
+//        NBTTagCompound tagCompound = null;
+//        if (bag.hasTagCompound())
+//        {
+//            tagCompound = bag.getTagCompound();
+//        }
+//        ItemStack output = new ItemStack(ModItems.itemGemBag,1,roseMeta+(netherrack?16:0));
+//
+//        if (tagCompound!=null)
+//        {
+//            output.setTagCompound(tagCompound);
+//        }
 //        if (tagCompound!=null)
 //        {
 //            output = new ItemStack(ModItems.itemGemBag,1,roseMeta+(netherrack?16:0),tagCompound);
